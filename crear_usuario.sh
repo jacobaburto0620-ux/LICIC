d0f224e9b6a1e7168e317cf7b125f3b5f70ec426
Jacob Pa <jacobaburto0620@gmail.com>
2026-04-09 00:48:12 -0600

    Usuarios Cuotas

--- crear_usuario.sh
+++ crear_usuario.sh
@@ -39,4 +39,33 @@ echo "$usuario:$pass" | sudo chpasswd
 
 echo "Usuario creado correctamente"
 
+#Creat cuotas 
+read -p "¿Dese asignar cuota ? S/N :" opcion
+ 
+if [ "$opcion" = "s" ]; then 
 
+	read -p "Soft limit (KB): " soft
+	read -p "Hard limit (KB):" hard
+
+	if [ "$soft" -ge "$hard" ]; then 
+		echo "El soft limit debe ser menor que el hard limit"
+		exit 1
+	fi
+
+	#aplicar cuota 
+	sudo setquota -u "$usuario" $soft $hard 0 0 /
+
+	echo "Cuota asignada"
+
+	read -p "¿Cambiar periodo de gracia? s/n " gracia 
+
+	if [ "$gracia" = "s" ]; then 
+		read -p "dias de gracia: " dias
+
+		sudo setquota -t $dias $dias /
+
+		echo "Periodo de gracia actualizado"
+	fi
+fi
+
+echo "proceso finalizado"
